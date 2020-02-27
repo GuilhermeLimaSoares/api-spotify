aff71c6e76ed232e2ea6337b7565adfdb8d6ddbc
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2020-02-26 22:16:05 -0300

    Componentizacao concluida com resposividade

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,33 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AlbumDetailComponent } from '../app/album-detail/album-detail.component';
-// import { FinalBuyComponent } from '../app/main-area/final-buy/final-buy.component';
-// import { CatalogComponent } from '../app/main-area/catalog/catalog.component';
-// import { ViewDetailComponent } from '../app/main-area/view-detail/view-detail.component';
-// import { ViewSearchComponent } from '../app/main-area/view-search/view-search.component';
-// import { MainAreaComponent} from '../app/main-area/main-area.component';
-// import { CardsComponent } from './header/icon-buttons/button-user/my-account/cards/cards.component';
-// import { MyAccountComponent } from './header/icon-buttons/button-user/my-account/my-account.component';
-// import { HomeComponent } from './main-area/home/home.component';
-// import { OrdersComponent } from './header/icon-buttons/button-user/my-account/orders/orders.component';
-// import { SingUpComponent } from './../app/header/icon-buttons/button-login/sing-up/sing-up.component';
-// import { FavoritesComponent } from './../app/main-area/favorites/favorites.component';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 
 const APP_ROUTES: Routes = [
-  // {path: '', component: AppComponent},
-  // {path: '', component: MainAreaComponent},
-//   {path: 'home', component: HomeComponent},
-//   {path: 'cadastro', component: SingUpComponent},
-//   {path: 'carrinho', component: BuyComponent},
-//   {path: 'catalogo', component: CatalogComponent},
-//   {path: 'compra', component: FinalBuyComponent },
-//   {path: 'detalhe', component: ViewDetailComponent},
-//   {path: 'favoritos', component: FavoritesComponent},
-//   // {path: 'cartoes', component: CardsComponent},
-//   {path: 'minhaconta', component: MyAccountComponent},
-//   {path: 'pesquisa', component: ViewSearchComponent},
-//   {path: 'pedidos', component: OrdersComponent},
   {path: 'home', component: AppComponent},
   {path: 'detalhes', component: AlbumDetailComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
